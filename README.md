aca9a3f9f7d9ff18d68927569b77608880f708e9
Early Ehlinger <early.ehlinger@clearcapital.com>
2014-08-04 07:45:37 -0500

    Latest

---
+++ README.md
@@ -0,0 +1,18 @@
+A set of emacs utilities. This is intended to replace your entire
+.emacs.d directory, because, well, it's my entire .emacs.d
+directory. Don't want to do that? Well, you could always fork and
+modify to your content. ;-)
+
+Installation:
+
+In a terminal:
+```bash
+git clone -r {url} ~/.emacs.d
+```
+
+In your ~/.emacs file:
+```lisp
+(load "~/.emacs.d/dotemacs")
+```
+
+That's it!
---
+++ earlye/earlye.el
@@ -0,0 +1,77 @@
+(defun ede-get-extension (name)
+	(replace-regexp-in-string
+	 ".*\\." 
+	 ""
+	 name 
+	 )
+)
+
+(defun ede-insert-file-header( )
+	(interactive)
+	(setq extension (ede-get-extension (buffer-name)))
+)
+
+(defun ede-insert-buffer-name( )
+  (interactive)
+  (insert (buffer-name) )
+)
+
+(defun ede-make-symbol (name)
+  (replace-regexp-in-string
+   "[\.\*]" "_"
+   name
+   )
+)
+
+(defun ede-buffer-name-symbol ( )
+  (upcase (ede-make-symbol (buffer-name)))
+)
+
+(defun ede-rtrim ( str )
+  (replace-regexp-in-string "[ \n\t\r]*$" "" str )
+)
+
+(defun ede-ltrim ( str )
+  (replace-regexp-in-string "^[ \t]*" "" str )
+)
+
+(defun ede-trim ( str )
+	( ede-ltrim ( ede-rtrim str ) )
+)
+
+(defun ede-insert-uuid( )
+(interactive)
+(setq id (ede-trim (shell-command-to-string "uuidgen" ) ))
+(insert id)
+)
+
+(defun ede-insert-uuid-hex( )
+(interactive)
+(setq id (ede-trim (shell-command-to-string "uuidgen | sed \"s/^/\\(0x/g;s/-/,0x/g;s/$/\\)/g\"" ) ))
+(insert id)
+)
+
+(defun ede-insert-translate-selection( )
+(interactive)
+(setq fnName (concat "str" (ede-trim (shell-command-to-string "uuidgen | sed s/-//g" )) "()"))
+(goto-char (region-end)) (insert (concat ");\n}\n  static std::string " fnName ";\n"))
+(goto-char (region-beginning)) (insert (concat "translations::" fnName "\nstd::string translations::" fnName "\n{\n  return tr("))
+)
+
+(defun ede-insert-cpp-header-guard ( )
+(interactive)
+(setq id (ede-trim (concat (ede-buffer-name-symbol) (shell-command-to-string "uuidgen | sed s/-/_/g ") )))
+(setq curYear (ede-trim (shell-command-to-string "date +%Y")))
+(beginning-of-buffer)
+(insert (concat "#ifndef " id "\n"
+"#define " id "\n"
+"\n// Copyright " curYear " EARLY EHLINGER\n"
+"\n"))
+
+(end-of-buffer)
+(insert (concat
+"\n"
+"#endif /* ndef " id " */\n\n" ))
+)
+
+(provide 'earlye)
